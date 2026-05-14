c32deb5dba528b418250163c2cb47c81d4b493a0
Cris Micheli <crismicheli.work@gmail.com>
2026-05-14 15:27:29 +0200

    Add project_graph to __all__ exports

--- src/parsy/graph/__init__.py
+++ src/parsy/graph/__init__.py
@@ -1,5 +1,5 @@
 from parsy.graph.builder import build_graph
 from parsy.graph.models import Edge, Node, PropertyGraph
+from parsy.graph.projection import project_graph
 
-__all__ = ["Edge", "Node", "PropertyGraph", "build_graph"]
-
+__all__ = ["Edge", "Node", "PropertyGraph", "build_graph", "project_graph"]
